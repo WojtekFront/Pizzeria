c97bf0d78e8492fb39f88ef2bdca9cb52f18dc3b
„wojtek <wojciech_lisowski@wp.pl>
2020-02-26 22:13:18 +0100

    add js

--- index.php
+++ index.php
@@ -63,7 +63,175 @@
             
 
             <div id="offert" class="mb-md-4 col-sm-12 table-responsive">
-            <button onclick="functionName()">Call the function</button>
+            <button type="button" onClick="function1()" href="#" role="button" id="offert">Call the function</button>
+            
+    <script>
+    function function1(){
+    document.write(
+        `
+        
+        <table class="table table-sm table-striped">
+                    <thead>
+                        <tr>
+                        <th scope="col" >lp</th>
+                        <th scope="col" >nazwa</th>
+                        <th scope="col">składniki</th>
+                        <th scope="col">mała</th>
+                        <th scope="col">duża</th>
+                        <th scope="col">gigant</th>
+                        
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <th scope="row">1</th>
+                        <td>MARGHERITA </td>
+                        <td>ser</td>
+                        <td>15,00</td>
+                        <td>20,00</td>
+                        <td>25,00</td>
+                        </tr>
+                        
+                        <tr>
+                        <th scope="row" class="secLine">2</th>
+                        <td>MAFFIA</td>
+                        <td>ser, szynka, pieczarki, szynka wędzona, cebula</td>
+                        <td>20,00</td>
+                        <td>24,00</td>
+                        <td>35,00</td>
+                        </tr>
+                        
+                        <tr>
+                        <th scope="row">3</th>
+                        <td>PEPERONI</td>
+                        <td>sos chilli, ser, szynka, papryka peperoni, pieczarki, szynka wędzona, oliwki, salami peperoni, papryczki jalapenio</td>
+                        <td>25,00</td>
+                        <td>28,00</td>
+                        <td>40,00</td>
+                        </tr>
+                        
+                        <tr>
+                        <th scope="row">4</th>
+                        <td>SZPINAKOWA</td>
+                        <td>sos szpinakowy, ser, suszone pomidory, ser sałatk., świeża bazylia</td>
+                        <td>20,00</td>
+                        <td>22,00</td>
+                        <td>41,00</td>
+                        </tr>
+
+                        <tr>
+                        <th scope="row">5</th>
+                        <td>WIEJSKA</td>
+                        <td>sos szpinakowy, ser, suszone pomidory, ser sałatk., świeża bazylia</td>
+                        <td>20,00</td>
+                        <td>22,00</td>
+                        <td>41,00</td>
+                        </tr>
+
+                        <tr>
+                        <th scope="row">6</th>
+                        <td>Wiejska</td>
+                        <td>ser, boczek X 2 , kiełbasa, cebula, ogórek,</td>
+                        <td>20,00</td>
+                        <td>22,00</td>
+                        <td>41,00</td>
+                        </tr>
+
+                        <tr>
+                        <th scope="row">7</th>
+                        <td>Wiejska</td>
+                        <td>ser, boczek X 2 , kiełbasa, cebula, ogórek,</td>
+                        <td>20,00</td>
+                        <td>22,00</td>
+                        <td>41,00</td>
+                        </tr>
+                        
+                        <tr>
+                        <th scope="row">8</th>
+                        <td>Wiejska</td>
+                        <td>ser, boczek X 2 , kiełbasa, cebula, ogórek,</td>
+                        <td>20,00</td>
+                        <td>22,00</td>
+                        <td>41,00</td>
+                        </tr>
+                        
+                        <tr>
+                        <th scope="row">9</th>
+                        <td>Wiejska</td>
+                        <td>ser, boczek X 2 , kiełbasa, cebula, ogórek,</td>
+                        <td>20,00</td>
+                        <td>22,00</td>
+                        <td>41,00</td>
+                        </tr>
+                        
+                        <tr>
+                        <th scope="row">10</th>
+                        <td>Wiejska</td>
+                        <td>ser, boczek X 2 , kiełbasa, cebula, ogórek,</td>
+                        <td>20,00</td>
+                        <td>22,00</td>
+                        <td>41,00</td>
+                        </tr>
+                        
+                        <tr>
+                        <th scope="row">11</th>
+                        <td>Wiejska</td>
+                        <td>ser, boczek X 2 , kiełbasa, cebula, ogórek,</td>
+                        <td>20,00</td>
+                        <td>22,00</td>
+                        <td>41,00</td>
+                        </tr>
+                        
+                        <tr>
+                        <th scope="row">12</th>
+                        <td>Wiejska</td>
+                        <td>ser, boczek X 2 , kiełbasa, cebula, ogórek,</td>
+                        <td>20,00</td>
+                        <td>22,00</td>
+                        <td>41,00</td>
+                        </tr>
+                        
+                        <tr>
+                        <th scope="row">13</th>
+                        <td>Wiejska</td>
+                        <td>ser, boczek X 2 , kiełbasa, cebula, ogórek,</td>
+                        <td>20,00</td>
+                        <td>22,00</td>
+                        <td>41,00</td>
+                        </tr>
+                        
+                        <tr>
+                        <th scope="row">14</th>
+                        <td>Wiejska</td>
+                        <td>ser, boczek X 2 , kiełbasa, cebula, ogórek,</td>
+                        <td>20,00</td>
+                        <td>22,00</td>
+                        <td>41,00</td>
+                        </tr>
+                        
+                        <tr>
+                        <th scope="row">15</th>
+                        <td>Wiejska</td>
+                        <td>ser, boczek X 2 , kiełbasa, cebula, ogórek,</td>
+                        <td>20,00</td>
+                        <td>22,00</td>
+                        <td>41,00</td>
+                        </tr>
+                        
+                        <tr>
+                        <th scope="row">16</th>
+                        <td>Wiejska</td>
+                        <td>ser, boczek X 2 , kiełbasa, cebula, ogórek,</td>
+                        <td>20,00</td>
+                        <td>22,00</td>
+                        <td>41,00</td>
+                        </tr>
+                                                
+                    </tbody>
+                </table>`
+        
+    )};
+</script>
             </div> <!--close offert-->
 
             <div id="contact" class="col-sm-12 ">
